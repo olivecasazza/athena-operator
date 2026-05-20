df919bea7eea5a74cf2e88179b9c27ffeeb05272
Olive Casazza <olive.casazza@gmail.com>
2026-05-20 01:36:08 +0000

    fix(nix): expose hydraJobs

--- flake.nix
+++ flake.nix
@@ -5,13 +5,22 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in {
-        packages = {
-          # Define image build targets here for Hydra
-          default = pkgs.hello;
-        };
-      });
+    let
+      # Use flake-utils for per-system outputs
+      outputs = flake-utils.lib.eachDefaultSystem (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in {
+          packages = {
+            # Define image build targets here for Hydra
+            default = pkgs.hello;
+          };
+        });
+    in outputs // {
+      # Hydra specifically looks for `hydraJobs` at the top level
+      hydraJobs = {
+        x86_64-linux = outputs.packages.x86_64-linux;
+        aarch64-linux = outputs.packages.aarch64-linux;
+      };
+    };
 }
