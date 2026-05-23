fa3491d94ad5812da994917aa4fa3d4c8e7b3926
Olive Casazza <olive@casazza.info>
2026-05-23 03:16:03 +0000

    fix(helm): escape namespace literal in grafana dashboard json to avoid helm templating errors

--- nix/athena/deployment.nix
+++ nix/athena/deployment.nix
@@ -7,8 +7,8 @@ let
     chart = {
       name = "athena";
       description = "Athena Kubernetes Research Operator Template";
-      version = "0.1.5";
-      appVersion = "0.1.5";
+      version = "0.1.6";
+      appVersion = "0.1.6";
     };
 
     namespace = "athena";
